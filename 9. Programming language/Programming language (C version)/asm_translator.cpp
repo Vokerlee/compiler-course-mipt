e6b0d1ec8473b4b300f87aafba792ec862ca45a1
Roman Glaz <70313579+Vokerlee@users.noreply.github.com>
2021-05-06 22:22:36 +0300

    Update asm_translator.cpp

--- 9. Programming language/Programming language (C version)/asm_translator.cpp
+++ 9. Programming language/Programming language (C version)/asm_translator.cpp
@@ -36,7 +36,7 @@ void lang_asm (FILE *formula)
 void analyse_expr (bin_tree_elem *element, FILE *assmbl, variables *var)
 {
     if (element->type == NUM)
-        fprintf(assmbl, "push %lg // push the number\n\n", element->value);
+        fprintf(assmbl, "push %d // push the number\n\n", (int) element->value);
     else if (element->type == GLOB_VAR)
     {
         fprintf(assmbl, "// push the global variable:\n"
